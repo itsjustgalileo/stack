a48fb0ad385c86236279574596bd953f7f7fe671
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2025-12-14 05:12:10 -0500

    Update and rename quest_stack.c to stack.c

--- stack.c
+++ stack.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 
 #include "./stack.h"
-#include "../strings/quest_string.h"
 
-struct quest_stack {
+struct stack {
     void *elems;
     size_t elem_size;
     size_t capacity; /* stack size */
@@ -12,8 +12,8 @@ struct quest_stack {
     /* bool dynamic; just a thought for later */
 };
 
-quest_stack_t *quest_stack_init(size_t size, size_t count) {
-    quest_stack_t *s = (quest_stack_t *)malloc(sizeof(quest_stack_t));
+Stack *stack_init(size_t size, size_t count) {
+    Stack *s = (Stack *)malloc(sizeof(Stack));
     if (NULL == s) {
         fprintf(stderr, "[stack_init]: Failed to allocate memory for stack\n");
         return NULL;
@@ -28,18 +28,18 @@ quest_stack_t *quest_stack_init(size_t size, size_t count) {
     }
     s->size = 0;
 #ifndef NDEBUG
-    my_memset(s->elems, 0xAD, s->elem_size * s->capacity);
+    memset(s->elems, 0xAD, s->elem_size * s->capacity);
 #endif /* NDEBUG */
     return s;
 }
 
-void quest_stack_cleanup(quest_stack_t *s) {
+void stack_cleanup(Stack *s) {
     assert(s);
     free(s->elems);
     free(s);
 }
 
-bool quest_stack_push(quest_stack_t *s, void *elem) {
+bool stack_push(Stack *s, void *elem) {
     assert(s && elem);
     if (s->size >= s->capacity) {
         /*
@@ -58,39 +58,39 @@ bool quest_stack_push(quest_stack_t *s, void *elem) {
     }
 
     void *dest = (char *)s->elems + s->size * s->elem_size;
-    my_memcpy(dest, elem, s->elem_size);
+    memcpy(dest, elem, s->elem_size);
     ++s->size;
     return true;
 }
 
-bool quest_stack_pop(quest_stack_t *s, void *out_elem) {
+bool stack_pop(Stack *s, void *out_elem) {
     assert(s && s->size && out_elem);
     --s->size;
     void *src = (char *)s->elems + s->size * s->elem_size;
-    my_memcpy(out_elem, src, s->elem_size);
+    memcpy(out_elem, src, s->elem_size);
     return true;
 }
 
-size_t quest_stack_size(quest_stack_t *s) {
+size_t stack_size(Stack *s) {
     assert(s);
     return s->size;
 }
 
-bool quest_stack_is_empty(quest_stack_t *s) {
+bool stack_is_empty(Stack *s) {
     assert(s);
     return s->size == 0;
 }
 
-bool quest_stack_reset(quest_stack_t *s) {
+bool stack_reset(Stack *s) {
     assert(s);
 #ifndef NDEBUG
-    my_memset(s->elems, 0xDE, s->elem_size * s->capacity);
+    memset(s->elems, 0xDE, s->elem_size * s->capacity);
 #endif /* NDEBUG */
     s->size = 0;
     return true;
 }
 
-void *quest_stack_top(quest_stack_t *s) {
+void *stack_top(Stack *s) {
     assert(s && s->size);
     return (char *)s->elems + (s->size - 1) * s->elem_size;
 }
