17ae958cfa672751c9e009b1c37b27da1be56b05
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2025-12-14 05:08:05 -0500

    Update quest_stack.h

--- quest_stack.h
+++ quest_stack.h
@@ -1,31 +1,17 @@
-#ifndef QUEST_STACK_H_
-#define QUEST_STACK_H_
+#ifndef STACK_H_
+#define STACK_H_
 
-#include <quest/quest_common.h>
+typedef struct Stack Stack;
 
-#include <quest/quest_header.h>
- 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
+Stack *stack_init(size_t size, size_t count);
+void stack_cleanup(Stack *s);
 
-    typedef struct quest_stack quest_stack_t;
+bool stack_push(Stack *s, void *elem);
+bool stack_pop(Stack *s, void *out_elem);
 
-    extern QUEST_API quest_stack_t *QUEST_DECL quest_stack_init(size_t size, size_t count);
-    extern QUEST_API void QUEST_DECL quest_stack_cleanup(quest_stack_t *s);
+size_t stack_size(Stack *s);
+bool stack_is_empty(Stack *s);
+bool stack_reset(Stack *s);
+void *stack_top(Stack *s);
 
-    extern QUEST_API bool QUEST_DECL quest_stack_push(quest_stack_t *s, void *elem);
-    extern QUEST_API bool QUEST_DECL quest_stack_pop(quest_stack_t *s, void *out_elem);
-
-    extern QUEST_API size_t QUEST_DECL quest_stack_size(quest_stack_t *s);
-    extern QUEST_API bool QUEST_DECL quest_stack_is_empty(quest_stack_t *s);
-    extern QUEST_API bool QUEST_DECL quest_stack_reset(quest_stack_t *s);
-    extern QUEST_API void *QUEST_DECL quest_stack_top(quest_stack_t *s);
-    
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-#include <quest/quest_footer.h>
-
-#endif /* QUEST_STACK_H_ */
+#endif /* STACK_H_ */
